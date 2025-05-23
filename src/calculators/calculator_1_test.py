91dc8b69928ec606ccd44216d691b24fcb69d158
Guilherme Jorge Hauck <78877899+Guihauck@users.noreply.github.com>
2025-05-23 10:05:39 -0300

    error testing

--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -1,5 +1,6 @@
 from .calculator_1 import Calculator1
 from typing import Dict
+from pytest import raises
 
 class MockRequest:
     def __init__(self, body: Dict) -> None:
@@ -10,9 +11,6 @@ def test_calculator():
     calculator_1 = Calculator1()
     response = calculator_1.calculate(mock_request)
 
-    print()
-    print(response)
-
     # Response format
     assert "data" in response
     assert "calculator" in response["data"]
@@ -21,3 +19,12 @@ def test_calculator():
     # Assertiveness of the response
     assert response["data"]["calculator"] == 1
     assert response["data"]["result"] == 14.25
+
+def test_calculate_with_body_error():
+    mock_request = MockRequest(body={ "something": 1 })
+    calculator_1 = Calculator1()
+
+    with raises(Exception) as excepinfo:
+        calculator_1.calculate(mock_request)
+    assert str(excepinfo.value) == "body mal formatado!"
+   
