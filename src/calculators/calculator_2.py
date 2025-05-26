205610542f9083a3eb6c0378fce3a8c855ef40c6
Guilherme Jorge Hauck <78877899+Guihauck@users.noreply.github.com>
2025-05-26 11:36:28 -0300

    Finalizing the Logic of the Second Calculator

--- src/calculators/calculator_2.py
+++ src/calculators/calculator_2.py
@@ -1,14 +1,32 @@
 from flask import request as FlaskRequest
 from typing import Dict, List
+from src.drivers.numpy_handler import NumpyHandler
 
 class Calculator2:
     def calculate(self, request:FlaskRequest) -> Dict: # type: ignore
         body = request.json
         input_data = self.__validate_body(body)
+        calculated_number = self.__process_data(input_data)
+        formated_result = self.__format_response(calculated_number)
+        return formated_result
 
     def __validate_body(self, body:Dict) -> List[float]:
         if "numbers" not in body:
             raise Exception("body mal formatado!")
         
         input_data = body["numbers"]
-        return input_data 
+        return input_data
+    
+    def __process_data(self, input_data: List[float]) -> float:
+        num_handler = NumpyHandler()
+        first_process_result = [(num * 11) ** 0.95 for num in input_data]
+        result = num_handler.standard_deviation(first_process_result)
+        return 1/result
+    
+    def __format_response(self, calculated_number: float) -> Dict:
+        return {
+            "data": {
+                "calculator": 2,
+                "result": round(calculated_number, 2).item()
+            }
+        }
