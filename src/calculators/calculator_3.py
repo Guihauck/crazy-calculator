debd3ffccb12636a464fa258f3149302eabb78bb
Guilherme Jorge Hauck <78877899+Guihauck@users.noreply.github.com>
2025-06-03 21:49:36 -0300

    Finishing Third Calculator Logic

---
+++ src/calculators/calculator_3.py
@@ -0,0 +1,45 @@
+from flask import request as FlaskRequest
+from typing import Dict, List
+from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
+
+class Calculator3:
+    def __init__(self, drive_handler: DriverHandlerInterface) -> None:
+        self.__drive_handler = drive_handler
+
+    def calculate(self, request: FlaskRequest) -> Dict: # type: ignore
+        body = request.json
+        input_data = self.__validate_body(body)
+        variance = self.__calculate_variance(input_data)
+        multiplication = self.__calculate_multiplication(input_data)
+        self.__verify_results(variance, multiplication)
+        formated_response = self.__verify_results(multiplication)
+
+        return formated_response
+        
+    def __validate_body(self, body: Dict) -> List[float]:
+        if "numbers" not in body:
+            raise Exception("poorly formed body")
+        
+        input_data = body["numbers"]
+        return input_data
+    
+    def __calculate_variance(self, numbers: List[float]) -> float:
+        variance = self.__drive_handler.variance(numbers)
+        return variance
+    
+    def __calculate_multiplication(self, numbers: List[float]) -> float:
+        multiplication = 1
+        for num in numbers: multiplication *= num
+
+    def __verify_results(self, variance: float, multiplication: float) -> None:
+        if variance < multiplication:
+            raise Exception("Process failure: Variance less than multiplication")
+        
+    def __format_response(self, variance: float) -> Dict:
+        return {
+            "data": {
+                "calculator": 3,
+                "value": variance,
+                "success": True
+            }
+        }
--- src/drivers/numpy_handler.py
+++ src/drivers/numpy_handler.py
@@ -6,5 +6,8 @@ class NumpyHandler(DriverHandlerInterface):
     def __init__(self):
         self.__np = numpy
     
-    def standard_deviation(self, numbers:List[float]) -> float:
+    def standard_deviation(self, numbers: List[float]) -> float:
         return self.__np.std(numbers)
+    
+    def variance(self, numbers: List[float]) -> float:
+        return self.__np.var(numbers)
